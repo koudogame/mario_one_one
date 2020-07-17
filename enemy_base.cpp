bbe34e780506ab60ec4e0931dd9db3872bf1b2c3
佐々木敦宏 <slime_0829@docomo.ne.jp>
2020-07-17 10:38:38 +0900

    ノコノコが敵と当たったとき、体の向きが反転していなかったバグを修正しました。

--- enemy_base.cpp
+++ enemy_base.cpp
@@ -19,37 +19,48 @@ void EnemyBase::draw( int Texture, int ScreenOver )
 
     if( enemy_parts_.rect.bottom == kSize )
     {
-        if(burn_)
-        // RECT指定して描画を行う関数
-         DrawRectGraph(
-            texture_position_x, texture_position_y,                                 // 描画位置
-            (int&)enemy_parts_.rect.left, (int&)enemy_parts_.rect.top,              // 切り取り左上
-            (int&)enemy_parts_.rect.right, (int&)enemy_parts_.rect.bottom,          // 切り取り右下
-            Texture, TRUE, FALSE );                                                 // テクスチャネーム、透明度、反転
+        if( burn_ )
+            // RECT指定して描画を行う関数
+            DrawRectGraph(
+                texture_position_x, texture_position_y,                                 // 描画位置
+                (int&)enemy_parts_.rect.left, (int&)enemy_parts_.rect.top,              // 切り取り左上
+                (int&)enemy_parts_.rect.right, (int&)enemy_parts_.rect.bottom,          // 切り取り右下
+                Texture, TRUE, FALSE );                                                 // テクスチャネーム、透明度、反転
         else
-        // 180度回転
-        DrawRectRotaGraph(
-            texture_position_x, texture_position_y,
-            (int&)enemy_parts_.rect.left, (int&)enemy_parts_.rect.top,
-            (int&)enemy_parts_.rect.right, (int&)enemy_parts_.rect.bottom,
-            1.0, 3.14, Texture, TRUE, FALSE, FALSE );
+            // 180度回転
+            DrawRectRotaGraph(
+                texture_position_x, texture_position_y,
+                (int&)enemy_parts_.rect.left, (int&)enemy_parts_.rect.top,
+                (int&)enemy_parts_.rect.right, (int&)enemy_parts_.rect.bottom,
+                1.0, 3.14, Texture, TRUE, FALSE, FALSE );
     }
-    else 
+    else
     {
-        if(burn_)
-        // RECT指定して描画を行う関数
-        DrawRectGraph(
-            texture_position_x, texture_position_y - (kSize / 2),                   // 描画位置
-            (int&)enemy_parts_.rect.left, (int&)enemy_parts_.rect.top,              // 切り取り左上
-            (int&)enemy_parts_.rect.right, (int&)enemy_parts_.rect.bottom,          // 切り取り右下
-            Texture, TRUE, FALSE );                                                 // テクスチャネーム、透明度、反転
+        if( burn_ )
+        {
+            if( direction_ == 1 )
+                // RECT指定して描画を行う関数
+                DrawRectGraph(
+                    texture_position_x, texture_position_y - (kSize / 2),                   // 描画位置
+                    (int&)enemy_parts_.rect.left, (int&)enemy_parts_.rect.top,              // 切り取り左上
+                    (int&)enemy_parts_.rect.right, (int&)enemy_parts_.rect.bottom,          // 切り取り右下
+                    Texture, TRUE, TRUE );                                                  // テクスチャネーム、透明度、反転
+            else
+                DrawRectGraph(
+                    texture_position_x, texture_position_y - (kSize / 2),                   // 描画位置
+                    (int&)enemy_parts_.rect.left, (int&)enemy_parts_.rect.top,              // 切り取り左上
+                    (int&)enemy_parts_.rect.right, (int&)enemy_parts_.rect.bottom,          // 切り取り右下
+                    Texture, TRUE, FALSE );                                                 // テクスチャネーム、透明度、反転
+        }
         else
-        // 180度回転
-        DrawRectRotaGraph(
-            texture_position_x, texture_position_y,
-            (int&)enemy_parts_.rect.left, (int&)enemy_parts_.rect.top,
-            (int&)enemy_parts_.rect.right, (int&)enemy_parts_.rect.bottom,
-            1.0, 3.14, Texture, TRUE, FALSE, FALSE );
+        {
+            // 180度回転
+            DrawRectRotaGraph(
+                texture_position_x, texture_position_y,
+                (int&)enemy_parts_.rect.left, (int&)enemy_parts_.rect.top,
+                (int&)enemy_parts_.rect.right, (int&)enemy_parts_.rect.bottom,
+                1.0, 3.14, Texture, TRUE, FALSE, FALSE );
+        }
     }
 }
 
